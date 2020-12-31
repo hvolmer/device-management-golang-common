c235cef2a58f46854dfb4f395b44cadf07903187
hvolmer <5054691+hvolmer@users.noreply.github.com>
2020-12-31 14:59:25 -0700

    DeviceID to ObjectID

--- devicechange/devicechange.go
+++ devicechange/devicechange.go
@@ -8,7 +8,7 @@ type DeviceChange struct {
 	Comment     string             `json:"comment,omitempty" bson:"comment,omitempty"`
 	CompanyUUID string             `json:"companyUuid" bson:"companyUuid"`
 	Compliance  string             `json:"compliance,omitempty" bson:"compliance,omitempty"`
-	DeviceID    string             `json:"deviceId" bson:"deviceId"`
+	DeviceID    primitive.ObjectID `json:"deviceId" bson:"deviceId"`
 	Path        string             `json:"path" bson:"path"`
 	Timestamp   string             `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
 	Type        string             `json:"type" bson:"type"`
