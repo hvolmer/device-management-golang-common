cf67f9a5f65ca9f9c862b30937d7a39bb7e9c4fd
hvolmer <5054691+hvolmer@users.noreply.github.com>
2020-12-31 17:16:34 -0700

    Added compliance function to Device

--- device/device.go
+++ device/device.go
@@ -1,6 +1,13 @@
 package device
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"log"
+
+	"github.com/xeipuuv/gojsonschema"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/hvolmer/device-management-golang-common/devicechange"
+)
 
 // Device type represents the basics of a Device Management device, where the query includes a
 // type, not just an ID
@@ -12,4 +19,20 @@ type Device struct {
 	Configuration   Configuration      `json:"configuration" bson:"configuration"`
 	Shadow          Shadow             `bson:"shadow,omitempty" bson:"shadow"`
 	Type            primitive.ObjectID `bson:"type,omitempty" bson:"type"`
+}
+
+// GetComplianceForChange ...
+func (dev Device) GetComplianceForChange(ch devicechange.DeviceChange) string {
+	for _, ci := range dev.ComplianceItems {
+		if ci.Path == ch.Path {
+			schemaLoader := gojsonschema.NewGoLoader(ci.Schema)
+			documentLoader := gojsonschema.NewGoLoader(ch.Value)
+			result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+			if err != nil {
+				log.Println("Error validating compliance. [value]", ch.Value, "[schema]", ci.Schema)
+			}
+			log.Println("COMPLIANCE RESULT:", result)
+		}
+	}
+	return ""
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hvolmer/device-management-golang-common
 
 go 1.15
 
-require go.mongodb.org/mongo-driver v1.4.4
+require (
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	go.mongodb.org/mongo-driver v1.4.4
+)
