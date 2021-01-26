869890471a33d8e56905507ccf52e30f48f5a1f6
hvolmer <hvolmer@pepperdash.com>
2021-01-26 09:34:55 -0700

    Added WithType type for devices

--- device/device.go
+++ device/device.go
@@ -15,3 +15,15 @@ type Device struct {
 	Shadow          Shadow             `bson:"shadow,omitempty" bson:"shadow"`
 	Type            primitive.ObjectID `bson:"type,omitempty" bson:"type"`
 }
+
+// WithType represents the basics of a Device Management device, where the query includes a
+// type, not just an ID
+type WithType struct {
+	ID              primitive.ObjectID `json:"_id" bson:"_id"`
+	CompanyUUID     string             `json:"companyUuid" bson:"companyUuid"`
+	ComplianceItems []ComplianceItem   `json:"complianceItems" bson:"complianceItems"`
+	Connectivity    Connectivity       `bson:"connectivity" bson:"connectivity"`
+	Configuration   Configuration      `json:"configuration" bson:"configuration"`
+	Shadow          Shadow             `bson:"shadow,omitempty" bson:"shadow"`
+	Type            Type               `bson:"type,omitempty" bson:"type"`
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/hvolmer/device-management-golang-common
 go 1.15
 
 require (
-	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 
 	go.mongodb.org/mongo-driver v1.4.5
 )
