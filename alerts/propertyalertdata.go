9229393c7bf37fb16f11e363055c973a5dc9d895
hvolmer <hvolmer@pepperdash.com>
2021-01-26 17:19:05 -0700

    DM-1329 Changed Previous name

--- alerts/propertyalertdata.go
+++ alerts/propertyalertdata.go
@@ -4,12 +4,12 @@ import "github.com/hvolmer/device-management-golang-common/devicechange"
 
 // PropertyAlertData ...
 type PropertyAlertData struct {
-	Path                   string
-	NameWithGroup          string
-	Label                  string
-	LastState              string
-	CurrentChange          devicechange.MinimizedDeviceChange
-	UserChange             devicechange.MinimizedDeviceChange
-	UserChangeName         string
-	PreviousOppositeChange devicechange.MinimizedDeviceChange
+	Path           string
+	NameWithGroup  string
+	Label          string
+	LastState      string
+	CurrentChange  devicechange.MinimizedDeviceChange
+	UserChange     devicechange.MinimizedDeviceChange
+	UserChangeName string
+	PreviousChange devicechange.MinimizedDeviceChange
 }
