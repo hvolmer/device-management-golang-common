00038a0967afc6bf3752cf0efca07d92a9ed001a
hvolmer <hvolmer@pepperdash.com>
2021-01-28 02:01:12 -0700

    DM-1344 added location to SystemAlertData

--- alerts/systemalertdata.go
+++ alerts/systemalertdata.go
@@ -3,9 +3,18 @@ package alerts
 // SystemAlertData represents the system and company data in queued
 // alert messages
 type SystemAlertData struct {
-	CompanyUUID string
-	CompanyName string
-	SystemUUID  string
-	SystemName  string
-	Devices     []DeviceAlertData
+	CompanyUUID    string
+	CompanyName    string
+	SystemLocation PortalSystemLocation
+	SystemUUID     string
+	SystemName     string
+	Devices        []DeviceAlertData
+}
+
+// PortalSystemLocation ...
+type PortalSystemLocation struct {
+	Location string `json:"location" bson:"location"`
+	Building string `json:"building" bson:"building"`
+	Floor    string `json:"floor" bson:"floor"`
+	Room     string `json:"room" bson:"room"`
 }
