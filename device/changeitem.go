0c28cc306ecd90410b541e4edb37aea0e166f0ba
hvolmer <hvolmer@pepperdash.com>
2021-02-09 11:36:01 -0700

    Added ChangeItemPopulated

--- device/changeitem.go
+++ device/changeitem.go
@@ -16,3 +16,14 @@ type ChangeItem struct {
 	Status                  string             `bson:"status"`
 	Value                   interface{}        `bson:"value"`
 }
+
+// ChangeItemPopulated contains a properly-populated change item
+type ChangeItemPopulated struct {
+	ID                      primitive.ObjectID      `bson:"_id"`
+	Comment                 string                  `bson:"comment"`
+	ExpiresDate             time.Time               `bson:"expiresDate"`
+	PropertyDefinition      PropertyDefinition      `bson:"propertyDefinition"`
+	PropertyDefinitionGroup PropertyDefinitionGroup `bson:"propertyDefinitionGroup"`
+	Status                  string                  `bson:"status"`
+	Value                   interface{}             `bson:"value"`
+}
