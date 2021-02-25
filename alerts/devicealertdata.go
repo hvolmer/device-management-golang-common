aaa4e6a990cf147edcf24fb73218a649ba6c886f
hvolmer <hvolmer@pepperdash.com>
2021-02-25 16:55:54 -0700

    Changed Device ID back to string

--- alerts/devicealertdata.go
+++ alerts/devicealertdata.go
@@ -1,10 +1,8 @@
 package alerts
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 // DeviceAlertData represents the device data in alert emails
 type DeviceAlertData struct {
-	ID         primitive.ObjectID `bson:"_id"`
+	ID         string `bson:"_id"`
 	Name       string
 	Properties []PropertyAlertData
 }
